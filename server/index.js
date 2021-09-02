258a6d2d8679d46b3a2bac09caaf92ac2a7df8ea
kappyman <alonkaplanski@gmail.com>
2021-09-02 11:19:32 +0300

    Add env file support, serve react index on routes that were not resolved before

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
+require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const app = express();
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.json);
 
 const PORT = process.env.PORT || 3001;
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!"});
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
